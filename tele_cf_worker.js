478cdc9c17e98af20b340e0313efa651a01d27fb
ourtt <40882650+ourtt@users.noreply.github.com>
2021-03-14 12:48:03 +0800

    Create tele_cf_worker.js

---
+++ tele_cf_worker.js
@@ -0,0 +1,31 @@
+//https://*.*.workers.dev/bot[your telegram bot token]/sendMessage?chat_id=[your telegram user id]&text=[message]
+const whitelist = ["/bot********:"];
+const tg_host = "api.telegram.org";
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+function validate(path) {
+    for (var i = 0; i < whitelist.length; i++) {
+        if (path.startsWith(whitelist[i]))
+            return true;
+    }
+    return false;
+}
+
+async function handleRequest(request) {
+    var u = new URL(request.url);
+    u.host = tg_host;
+    if (!validate(u.pathname)) {
+        return new Response('Unauthorized', {
+            status: 403
+        });
+    }
+    var req = new Request(u, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body
+    });
+    return await fetch(req);
+}
